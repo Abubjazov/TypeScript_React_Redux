f54d03fec9c474eb14b02549108780cf5f62e3c3
Radik Abubjazov <abubjazov@mail.ru>
2022-01-04 00:25:30 +0300

    app: add Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,15 @@
 import {Component} from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import { App } from './components/App'
+import { reducers } from './reducers'
 
-interface AppProps {
-    color?: string 
-}
-interface AppState {
-    counter: number
-}
+const store = createStore(reducers, applyMiddleware(thunk))
 
-const App = (props: AppProps): JSX.Element => {
-    return (
-        <div>{props.color}</div>
-    )
-}
-
-// class App extends Component<AppProps, AppState> {
-
-//     constructor(props: AppProps) {
-//         super(props)
-
-//         this.state = { counter: 0 }
-//     }
-
-//     onIncrement = (): void => {
-//         this.setState({ counter: this.state.counter + 1})
-//     }
-
-//     onDecrement = (): void => {
-//         this.setState({ counter: this.state.counter - 1})
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <button onClick={ this.onIncrement }>+</button>
-//                 <button onClick={ this.onDecrement }>-</button>
-//                 <h1>{ this.state.counter }</h1>
-//             </div>
-//         )
-//     }
-// }
-
-ReactDOM.render(<App color='red'/>, document.querySelector('#root'))
+ReactDOM.render(
+    <Provider store={ store }>
+        <App />
+    </Provider>
+, document.querySelector('#root'))
