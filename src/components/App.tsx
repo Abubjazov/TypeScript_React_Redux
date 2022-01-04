9e12a7840e40ad4abb012c076ffe8df51c9cf1e8
Radik Abubjazov <abubjazov@mail.ru>
2022-01-04 03:56:56 +0300

    app: +receiving and displaying data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,40 @@
 import { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchTodos, Todo } from '../actions'
+import { StoreState } from '../reducers/index'
 
-export class App extends Component {
+export interface AppProps {
+    todos: Todo[]
+    fetchTodos(): any
+}
+
+class _App extends Component<AppProps> {
+
+    onButtonClick = (): void => {
+        this.props.fetchTodos()
+    }
+
+    renderTodos(): JSX.Element[] {
+        return this.props.todos.map((todo: Todo) => {
+            return <div key={todo.id}>{todo.title}</div>
+        })
+    }
+
     render() {
         return (
             <div>
-                Hello!!! )))
+                <button onClick={this.onButtonClick}>Fetch</button>
+                {this.renderTodos()}
             </div>
         )
     }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+    return { todos }
+}
+
+export const App = connect(
+    mapStateToProps,
+    { fetchTodos }
+)(_App)
